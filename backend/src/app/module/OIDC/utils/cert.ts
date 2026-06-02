9c26442fc4b215fd8f72d1c68f326b58087f85ac
Sumitchauhan-co <chauhan.sumit3012@gmail.com>
2026-06-03 00:26:33 +0530

    fix: add fallback logic in cert.ts

--- backend/src/app/module/OIDC/utils/cert.ts
+++ backend/src/app/module/OIDC/utils/cert.ts
@@ -1,5 +1,10 @@
-import { readFileSync } from "node:fs";
-import path from "node:path";
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
 
-export const PRIVATE_KEY = readFileSync(path.resolve("cert/private-key.pem"));
-export const PUBLIC_KEY = readFileSync(path.resolve("cert/public-key.pub"));
+export const PRIVATE_KEY = process.env.OIDC_PRIVATE_KEY
+    ? process.env.OIDC_PRIVATE_KEY
+    : readFileSync(path.resolve('cert/private-key.pem'), 'utf8');
+
+export const PUBLIC_KEY = process.env.OIDC_PUBLIC_KEY
+    ? process.env.OIDC_PUBLIC_KEY
+    : readFileSync(path.resolve('cert/public-key.pub'), 'utf8');
